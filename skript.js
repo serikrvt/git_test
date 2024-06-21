a0f498a847fa3bc217a348e02f84f527cbd391bf
serikrvt <serikrvt@gmail.com>
2024-06-22 00:33:23 +0300

    test_2

---
+++ skript.js
@@ -0,0 +1,9 @@
+console.dir(document);
+const elementById = document.getElementById('container');
+console.log(elementById);
+const changeColorButton = document.getElementById('changeColorButton')
+console.log(changeColorButton);
+const elementText = document.getElementsByClassName('text')
+console.log(elementText);
+const ElementsByClassName = document.querySelectorAllg('li')
+console.log(ElementsByClassName);
